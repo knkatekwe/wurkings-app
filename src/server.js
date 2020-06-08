e51c4d68d00c1fbd1ce0b4b8cce68fc4e2a09c8b
knkatekwe <52075192+knkatekwe@users.noreply.github.com>
2020-06-08 18:14:52 +0200

    fixed cofiguration on npm

--- src/server.js
+++ src/server.js
@@ -4,5 +4,5 @@ const app = express();
 app.use(express.static(__dirname + '/dist/wurkings-app'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-'/dist/myapp/index.html'));});
+'/dist/wurkings-app/index.html'));});
 app.listen(process.env.PORT || 8080);
