5b1d8c45960c82bf1b2ecfa538184701525dcdd6
knkatekwe <52075192+knkatekwe@users.noreply.github.com>
2020-06-08 18:36:38 +0200

    fixed cofiguration on npm

--- src/server.js
+++ src/server.js
@@ -1,8 +1,7 @@
 const express = require('express');
-const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/wurkings-app'));
-app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+
-'/dist/wurkings-app/index.html'));});
+app.use(express.static('./dist'));
+app.get('/*', function (req, res) {
+ res.sendFile(__dirname + '/dist/index.html');
+});
 app.listen(process.env.PORT || 8080);
