4511960413731c3b26f0160f2c9cf62bde22b960
knkatekwe <52075192+knkatekwe@users.noreply.github.com>
2020-09-19 04:08:25 +0200

    checkout update

--- src/app/checkout/checkout-payme/checkout-payme.component.ts
+++ src/app/checkout/checkout-payme/checkout-payme.component.ts
@@ -1,5 +1,18 @@
 import { Component, OnInit, ElementRef, ViewChild, Input } from '@angular/core';
-import { Booking } from 'src/app/core';
+import { Observable } from 'rxjs';
+import { Category } from 'src/app/admin/state/category/category.model';
+import { CategoryQuery } from 'src/app/admin/state/category/category.query';
+import { CategoryService } from 'src/app/admin/state/category/category.service';
+import { Profile, UserService } from 'src/app/core';
+import { Booking } from 'src/app/core/state/booking/booking.model';
+import { BookingQuery } from 'src/app/core/state/booking/booking.query';
+import { BookingService } from 'src/app/core/state/booking/booking.service';
+import { PaymentRate } from 'src/app/core/state/payment-rate/payment-rate.model';
+import { PaymentRateQuery } from 'src/app/core/state/payment-rate/payment-rate.query';
+import { PaymentRateService } from 'src/app/core/state/payment-rate/payment-rate.service';
+import { Listing } from 'src/app/listing/state/listing.model';
+import { ListingQuery } from 'src/app/listing/state/listing.query';
+import { ListingService } from 'src/app/listing/state/listing.service';
 
 declare var paypal;
 
@@ -18,18 +31,49 @@ export class CheckoutPaymeComponent implements OnInit {
 
   paymentDetail: any;
 
+  owner: Profile;
+  paymentRate$: Observable<PaymentRate>;
+  category$: Observable<Category>;
+  booking$: Observable<Booking>;
+  listing$: Observable<Listing>
+  listing: Listing;
+
+	constructor(
+		private userService: UserService,
+		private bookingService: BookingService,
+		private bookingQuery: BookingQuery,
+		private listingService: ListingService,
+		private listingQuery: ListingQuery,
+		private paymentRateService: PaymentRateService,
+		private paymentRateQuery: PaymentRateQuery,
+		private categoryService: CategoryService,
+		private categoryQuery: CategoryQuery,
+	) {}
+
+
   ngOnInit() {
+    this.listing$ = this.listingService.getListing(this.booking.listing_id);
+		this.paymentRate$ = this.paymentRateService.getPaymentRate(this.booking.payment_rate_id);
+		this.listing$.subscribe((res) => {
+      this.listing = res
+			this.userService.getProfile(res.user_id).subscribe((res) => {
+				this.owner = res;
+				// console.log('..this is the owner...')
+				// console.log(this.owner)
+			});
+    });
+    //paypal
     paypal
       .Buttons({
         createOrder: (data, actions) => {
           return actions.order.create({
             purchase_units: [
               {
-                description: this.booking.listing.description,
+                description: this.listing.description,
                 reference_id: this.booking.id,
                 amount: {
                   currency_code: 'USD',
-                  value: this.booking.total_amount
+                  value: this.booking.total
                 }
               }
             ]
--- src/app/shared/checkout-helpers/checkout-product/checkout-product.component.ts
+++ src/app/shared/checkout-helpers/checkout-product/checkout-product.component.ts
@@ -1,5 +1,17 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { Listing, Booking } from 'src/app/core';
+import { Observable } from 'rxjs';
+import { Category } from 'src/app/admin/state/category/category.model';
+import { CategoryService } from 'src/app/admin/state/category/category.service';
+import { Profile, UserService } from 'src/app/core';
+import { Booking } from 'src/app/core/state/booking/booking.model';
+import { BookingQuery } from 'src/app/core/state/booking/booking.query';
+import { BookingService } from 'src/app/core/state/booking/booking.service';
+import { PaymentRate } from 'src/app/core/state/payment-rate/payment-rate.model';
+import { PaymentRateQuery } from 'src/app/core/state/payment-rate/payment-rate.query';
+import { PaymentRateService } from 'src/app/core/state/payment-rate/payment-rate.service';
+import { Listing } from 'src/app/listing/state/listing.model';
+import { ListingQuery } from 'src/app/listing/state/listing.query';
+import { ListingService } from 'src/app/listing/state/listing.service';
 
 @Component({
   selector: 'app-checkout-product',
@@ -9,7 +21,34 @@ export class CheckoutProductComponent implements OnInit {
 
   @Input() booking: Booking;
 
+  owner: Profile;
+  paymentRate$: Observable<PaymentRate>;
+  category$: Observable<Category>;
+  booking$: Observable<Booking>;
+  listing$: Observable<Listing>
+
+	constructor(
+		private userService: UserService,
+		private bookingService: BookingService,
+		private bookingQuery: BookingQuery,
+		private listingService: ListingService,
+		private listingQuery: ListingQuery,
+		private paymentRateService: PaymentRateService,
+		private paymentRateQuery: PaymentRateQuery,
+		private categoryService: CategoryService,
+		private categoryQuery: CategoryService,
+	) {}
+
   ngOnInit() {
+    this.listing$ = this.listingService.getListing(this.booking.listing_id);
+		this.paymentRate$ = this.paymentRateService.getPaymentRate(this.booking.payment_rate_id);
+		this.listing$.subscribe((res) => {
+			this.userService.getProfile(res.user_id).subscribe((res) => {
+				this.owner = res;
+				// console.log('..this is the owner...')
+				// console.log(this.owner)
+			});
+		});
   }
 
 }
